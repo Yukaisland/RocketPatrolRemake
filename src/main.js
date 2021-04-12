9f94ad51f934b07ba4ca3939e9519dc18dd12d9a
Yukai Liu <yliu637@uscs.edu>
2021-04-11 23:11:36 -0700

    showing static starfield background

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [Menu, Play],
+    scene: [ Menu, Play ]
 }
 
 let game = new Phaser.Game(config);
 
-let borderUIsize = game.config.height / 15;
-let borderPadding = borderUIsize / 3;
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,25 +1,31 @@
 class Play extends Phaser.Scene{
     constructor(){
         super("playScene");
-    
     }
+
+    preload() {
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
     create() {
-        //green UI background
-        this.add.rectangle(
-        0, 
-        borderUIsize + borderUIsize, 
-        game.config.width, 
-        borderUIsize * 2,
-        0x00FF00,
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+        // green UI background
+        this.add.rectangle(0, borderUISize + borderPadding,
+             game.config.width, 
+             borderUISize * 2, 
+             0x00FF00
+             ).setOrigin(0, 0);
 
-        ).setOrigin(0,0);
-
-        //white borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-
+        // white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         
-    }
+
+        }
+        update() {
+            this.starfield.tilePositionX -=4;
+        }
 }
